09519a480380753f3df11541dd2c064a3b5d2387
Ethan Fraenkel <ethan.fraenkel@hotmail.com>
2018-08-17 15:35:12 +0200

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # get 'cocktails/index'
   # get 'cocktails/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "cocktails#index"
   resources :cocktails do
     resources :doses, only: [:new, :create, :destroy]
   end
